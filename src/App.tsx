2334c255dace70280a29c39e0199202148ca65ce
Dipen Khadka <dipenkhadka191@gmail.com>
2023-12-23 12:06:16 +0545

    feat: stored todos in a state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
+import InputField from "./Components/InputField"
+import { Todo } from "./model";
 
-type Props = {}
 
-const App = (props: Props) => {
+const App:React.FC = () => {
+
+  const [todo, settodo] = useState<string>("");
+  const [todos, settodos] = useState<Todo[]>([]);
+
+
+  const handleAdd = (e:React.FormEvent) =>{
+    e.preventDefault();
+    if(todo)
+    {
+      settodos([...todos,{id:Date.now(), todo:todo, isDone:false}])
+      settodo("");
+    }
+  } 
+
   return (
-    <div>App</div>
-  )
+      <div className="container mx-auto p-4" >
+          <h1 className=" font-medium text-2xl text-center">Tasks</ h1>
+          <InputField todo={todo} settodo={settodo} handleAdd={handleAdd} />
+      </div>
+    )
 }
 
 export default App
---
+++ src/Components/InputField.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+type Props = {
+    todo:string,
+    settodo:React.Dispatch<React.SetStateAction<string>>;
+    handleAdd : (e:React.FormEvent)=> void
+}
+
+const InputField = (props: Props) => {
+    return (
+        <form onSubmit={(e)=>props.handleAdd(e)} className='flex w-11/12 relative items-center mt-8' action="">
+            <input value={props.todo} onChange={(e)=>props.settodo(e.target.value)} type="input" placeholder='Enter Your Task' className='input__box text-black w-full rounded-3xl p-4 border-none' name="" id="" />
+            <button className='btn absolute right-2 bg-[#2f74c0;] rounded-full px-3 h-5/6 shadow-xl'>Add</button>
+        </form>
+    )
+}
+
+
+export default InputField
