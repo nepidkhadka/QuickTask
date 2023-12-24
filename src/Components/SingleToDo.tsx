6017bd18cee14674dbeabc220c9272c9a9e34c5a
Dipen Khadka <dipenkhadka191@gmail.com>
2023-12-24 19:59:08 +0545

    feat: added delete & mark as done functionality

--- src/Components/SingleToDo.tsx
+++ src/Components/SingleToDo.tsx
@@ -3,9 +3,6 @@ import { MdEditOff } from "react-icons/md";
 import { MdDeleteForever } from "react-icons/md";
 import { MdDoneAll } from "react-icons/md";
 
-
-
-
 type Props = {
     todo: Todo,
     todos: Todo[],
@@ -13,14 +10,30 @@ type Props = {
 }
 
 const SingleToDo = ({ todo, todos, settodos }: Props) => {
+
+    const handleDelete = (id: number) => {
+       settodos(todos.filter(todo=>todo.id!==id))
+    }
+
+    const handleDone = (id: number) => {
+        settodos(todos.map((todo) => (
+            todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+        )))
+    }
+
     return (
         <form className="border rounded-md flex grow w-full justify-between p-2" action="">
-            <span className="p-1 font-medium text-2xl overflow-auto rounded-md">{todo.todo}</span>
+            {
+                todo.isDone ?
+                    (<s className="p-1 font-medium text-2xl overflow-auto rounded-md">{todo.todo}</s>)
+                    :
+                    (<span className="p-1 font-medium text-2xl overflow-auto rounded-md">{todo.todo}</span>)
+            }
             <div className="flex gap-2 items-center">
-                <button className="bg-yellow-500 p-1 rounded-md"><MdEditOff /></button>
-                <button className="bg-red-500 p-1 rounded-md" ><MdDeleteForever /></button>
-                <button className="bg-green-500 p-1 rounded-md"  ><MdDoneAll />
-                </button>
+                <span className="bg-yellow-500 p-1 rounded-md"><MdEditOff /></span>
+                <span className="bg-red-500 p-1 rounded-md" onClick={() => handleDelete(todo.id)} ><MdDeleteForever /></span>
+                <span className="bg-green-500 p-1 rounded-md" onClick={() => handleDone(todo.id)}  ><MdDoneAll />
+                </span>
             </div>
         </form>
     )
